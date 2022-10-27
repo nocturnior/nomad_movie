abe8bcb39d05e3c1571b659dc267ffed65b4f105
MIINII <miinii@MIINIIs-Air.local>
2022-10-27 21:59:55 +0900

    :art: Style : module.css 사용해보기

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.module.css';
+
+// components
+import Button from './components/Button';
+
+const App = () => {
+  return (
+    <>
+      <h1 className={styles.title}>ㅋㅋ</h1>
+      <Button text={'버튼!!'} />
+    </>
+  );
+};
+
+export default App;
