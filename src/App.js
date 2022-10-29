991c461d882115ab9013d9957b830ec925b9482b
nocturnior <ssuperr_@naver.com>
2022-10-29 23:47:56 +0900

    :art: Feat : useEffect() 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './App.module.css';
 import Router from './Router';
 
@@ -7,14 +7,31 @@ import Button from './components/Button';
 
 const App = () => {
   const [counter, setValue] = React.useState(0);
+  const [keyword, setKeyword] = React.useState('');
+
   const onClick = () => setValue(prev => prev + 1);
-  console.log();
+  const onChange = e => {
+    setKeyword(e.target.value);
+  };
+
+  console.log('계속 실행하겟다!');
+
+  useEffect(() => {
+    console.log('에이피아이 불러오기!');
+    // 검색api이용할때 한번만 불러오고싶음~ 다른거할때는 안변화햇으면 좋겟ㅇ
+  }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('SEARCH', keyword);
+    }
+  }, [keyword]); // keyword가 변할때마다 실행~
 
   return (
     <>
-      <Router />
-      {/* <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={onClick} text={'버튼!!'} /> */}
+      {/* <Router /> */}
+      <input onChange={onChange} type='text' placeholder='검색' value={keyword} />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text={'버튼!!'} />
     </>
   );
 };
